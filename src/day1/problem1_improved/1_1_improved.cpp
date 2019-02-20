1315ca7b8c11e04ba828ec6497970132ae26b6c8
Takashi Oya <>
2019-02-20 23:05:53 +0900

    add comment

--- src/day1/problem1_improved/1_1_improved.cpp
+++ src/day1/problem1_improved/1_1_improved.cpp
@@ -9,12 +9,18 @@ int get_y_times_8(int x)
 int main()
 {
     int y, y_times_8;
+
     for(int x = 1 ; x <= 100; x++)
     {
+        // calculate 8 * y
         y_times_8 = get_y_times_8(x);
+        
+        // check y is integer
         if (y_times_8 % 8 == 0)
         {
             y = y_times_8/8;
+            
+            // check y is in [1, 100]
             if(y >= 1 && y <= 100)
             {
                 std::cout << "x = " << x << " ";

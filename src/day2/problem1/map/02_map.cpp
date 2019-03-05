2ec2e07dee917ecca22eb248856956badd021ff4
Takashi Oya <>
2019-03-06 03:38:18 +0900

    add 02_map.cpp

---
+++ src/day2/problem1/map/02_map.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+    // 宣言
+    map<char, int> mp; 
+
+    // valueの設定、参照
+    mp['a'] = 4; // valueの設定
+    mp['b'] = 5;
+    cout << mp['a'] << endl; // 値の参照
+    mp['a'] = 3; // valueの上書き
+    cout << mp['a'] << endl;
+    cout << "全要素の表示" << endl;
+    for(auto itr = mp.begin(); itr != mp.end(); ++itr) // 全要素の表示
+    {
+        cout << "key = " << itr->first << ", value = " << itr->second << endl;
+    }
+
+    // keyの検索
+    auto itr = mp.find('a'); // keyが設定されていれば対応するiteratorが、設定されていなければend()へのiteratorが帰る
+    cout << "検索結果" << endl;
+    cout << "key = " << itr->first << ", value = " << itr->second << endl;
+
+    // 要素の削除
+    mp.erase('a'); // keyを指定して削除
+    mp.erase(mp.begin()); // iteratorを指定して削除
+    cout << "全要素の表示" << endl;
+    for(auto itr = mp.begin(); itr != mp.end(); ++itr) // 全要素の表示
+    {
+        cout << "key = " << itr->first << ", value = " << itr->second << endl;
+    }
+
+}

4230b1425e99a614320943b1250595f0e535fe71
Takashi Oya <>
2019-03-06 03:57:47 +0900

    update 01_list.cpp

--- src/day2/problem1/list/01_list.cpp
+++ src/day2/problem1/list/01_list.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {   
-    // 要素の宣言・初期化まとめ
+    // 宣言・初期化
     list<int> lst; // int型で宣言
     list<int> lst_2(123); // 要素数を指定して宣言
     list<int> lst_3(10, 5); // 要素数10, 全ての値を5として宣言
     list<int> lst_4{4, 6, 5}; // データ列を指定して宣言
 
-    // 要素の参照・代入まとめ
+    // 参照・代入
     auto itr = lst_4.begin(); // 最初の要素を示すiteratorの取得
     cout << "最初の要素を表示" << endl << *itr << endl; // 最初の要素を表示
     *itr = 9; // 要素の代入
@@ -20,7 +20,7 @@ int main()
         cout << *itr_2 << endl;
     }
 
-    // 要素の追加まとめ
+    // 要素の追加
     lst_4.push_front(11); // データを先頭に追加
     lst_4.push_back(15); // データを末尾に追加
     lst_4.insert(lst_4.begin(), 10); // iteratorを用いた要素のinsert
@@ -30,7 +30,7 @@ int main()
         cout << *itr_2 << endl;
     }
 
-    // 要素の削除まとめ
+    // 要素の削除
     lst_4.pop_front(); // 先頭の要素を削除
     lst_4.pop_back(); // 最後の要素を削除
     lst_4.erase(lst_4.begin()); // iteratorを用いた要素の削除

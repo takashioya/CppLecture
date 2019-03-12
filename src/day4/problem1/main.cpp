39a08065fc4b5355bb9767b5b24841f2fdd2bd28
Takashi Oya <>
2019-03-13 07:24:49 +0900

    add comment

--- src/day4/problem1/main.cpp
+++ src/day4/problem1/main.cpp
@@ -7,7 +7,6 @@ void swap_1(const int &i, const int &j) // const int &hoge
     int tmp = i;
     i = j; 
     j = tmp;
-    // i, j はmodifiableでないためエラーが出る
     cout << "&i: " << &i << ", &j: " << &j << endl;
 }
 
@@ -20,7 +19,6 @@ void swap_2(const int *i_ptr, const int *j_ptr) // int *const hoge
     int tmp = *i_ptr;
     *i_ptr = *j_ptr;
     *j_ptr = tmp;
-    // 同様に、*i_ptr, *j_ptrはmodifiableでないためエラーが出る
 }
 */
 
@@ -44,12 +42,27 @@ int main()
     swap_2(&x, &y)
     */
     swap_3(&x, &y); // これはエラーを吐かない
+
     /*
-    理由:
-    swap_1の書き方(const int &hoge)だと、hogeは変更不可能
-    swap_2の書き方(const int *hoge)だと、ポインタの指す値は変更不能だが、ポインタ自体は変更可能
-    swap_3の書き方(int *const hoge)だと、ポインタの指す値は変更可能だが、ポインタ自体は変更不可能
+    理由の説明:
+
+    swap_1の書き方(const int &hoge)だと、hogeは変更不可能であるため、
+    i = j; 
+    j = tmp;
+    の部分がアウト。
+    
+    swap_2の書き方(const int *hoge)だと、ポインタ自体は変更可能だが、ポインタの指す値は変更不能なので、
+    *i_ptr = *j_ptr;
+    *j_ptr = tmp;
+    の部分がアウト。
+
+    swap_3の書き方(int *const hoge)だと、ポインタ自体は変更不能だが、ポインタの指す値は変更可能なので、
+    *i_ptr = *j_ptr;
+    *j_ptr = tmp;
+    の部分はOK。
+
     */
+   
     cout << "x= " << x << ", y= " << y << endl;
 
 }
